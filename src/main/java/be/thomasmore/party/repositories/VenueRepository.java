6798b260a527cd97b3fbaf4284148902b03014f6
seppe <r0787714@thomasmore.be>
2020-10-22 11:21:05 +0200

    VenyeRepository import verwijderd

--- src/main/java/be/thomasmore/party/repositories/VenueRepository.java
+++ src/main/java/be/thomasmore/party/repositories/VenueRepository.java
@@ -4,8 +4,6 @@ import be.thomasmore.party.model.Venue;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface VenueRepository extends CrudRepository<Venue, Integer> {
