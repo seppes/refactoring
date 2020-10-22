bfa9d6ae7621640c5904988c89e5c2523f92e8ad
seppe <r0787714@thomasmore.be>
2020-10-22 11:29:25 +0200

    PartyApplication comments verwijderd

--- src/main/java/be/thomasmore/party/PartyApplication.java
+++ src/main/java/be/thomasmore/party/PartyApplication.java
@@ -3,11 +3,6 @@ package be.thomasmore.party;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-/*
-@EnableJpaRepositories("be.thomasmore.party.repositories")
-@EntityScan("be.thomasmore.party.model")
-*/
 @SpringBootApplication
 public class PartyApplication {
 
